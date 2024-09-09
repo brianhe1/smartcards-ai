bc300b4198434fa4c1a4183d577a99f91dbef04d
Brian He <brianhe0507@gmail.com>
2024-09-09 16:14:25 -0400

    changed name for sign in

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -29,7 +29,7 @@ const theme = createTheme({
   });
   
 
-export default function SignUpPage() {
+export default function SignInPage() {
     const {isLoaded} = useUser()
     const [loading, setLoading] = useState(true);
 
